d1b6153438398b5f4bb74477ddf6354b01fe8730
Xiaoyu Wang <wxybkaphsc@gmail.com>
2023-02-09 16:54:22 -0500

    feb 9 B0 HF

--- B0/proj/run_hf.jl
+++ B0/proj/run_hf.jl
@@ -30,7 +30,7 @@ save(joinpath(fpath,"data/$(prefix)_strain_hf_nu2.jld2"),"hf",hf);
 hf = load(joinpath(fpath,"data/1_strain_hf_nu2.jld2"),"hf");
 kvec = reshape(latt.kvec ./ abs(params.g1),lk,lk)
 ϵ0 = reshape(hf.ϵk,hf.nt,lk,lk)
-# plot_contour_maps(kvec,ϵ0[6,:,:],points=[params.Kt/abs(params.g1)])
+plot_contour_maps(kvec,ϵ0[5,:,:],points=[params.Kt/abs(params.g1)])
 iΓ = (lk%2==0) ? (lk÷2) : ((lk-1)÷2+1)
 kcut = real(kvec[:,iΓ])
 Ecut = ϵ0[:,:,iΓ]
