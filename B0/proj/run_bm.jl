8925edb2f5c910001c82bf53562826cbc28085cd
Xiaoyu Wang <wxybkaphsc@gmail.com>
2023-02-10 17:48:53 -0500

    feb 10

--- B0/proj/run_bm.jl
+++ B0/proj/run_bm.jl
@@ -6,12 +6,12 @@ include(joinpath(fpath,"libs/plot_helpers.jl"))
 
 # ------------------ Specification ------------------ #
 lk = 19
-params = Params(ϵ=0.0,Da=00,dθ=1.06π/180,w1=110,w0=77,vf=2482)
+params = Params(ϵ=0.002,Da=-4100,dθ=1.06π/180,w1=110,w0=77,vf=2482)
 initParamsWithStrain(params)
 latt = Lattice()
 initLattice(latt,params;lk=lk)
 
-bm_path = joinpath(fpath,"data/_bm_lk$(lk).jld2")
+bm_path = joinpath(fpath,"data/strain_bm_lk$(lk).jld2")
 
 # ------------------ non-interacting part ------------------ #
 function compute_bm(latt::Lattice,params::Params;fname::String="placeholder.txt")
@@ -25,10 +25,10 @@ bm = compute_bm(latt,params,fname=bm_path);
 
 # ------------------ non-interacting analysis ------------------ #
 
-kvec = reshape(latt.kvec ./ abs(params.g1),lk,lk)
-ϵ0 = reshape(load(bm_path,"E"),:,lk,lk)
-plot_contour_maps(kvec,ϵ0[8,:,:],points=[params.Kt/abs(params.g1)])
-iΓ = (lk%2==0) ? (lk÷2) : ((lk-1)÷2+1)
-kcut = real(kvec[:,iΓ])
-Ecut = reshape(ϵ0[:,:,iΓ],:,length(kcut))
-plot_energy_cuts(kcut,Ecut,lines=[0.0])
+# kvec = reshape(latt.kvec ./ abs(params.g1),lk,lk)
+# ϵ0 = reshape(load(bm_path,"E"),:,lk,lk)
+# plot_contour_maps(kvec,ϵ0[8,:,:],points=[params.Kt/abs(params.g1)])
+# iΓ = (lk%2==0) ? (lk÷2) : ((lk-1)÷2+1)
+# kcut = real(kvec[:,iΓ])
+# Ecut = reshape(ϵ0[:,:,iΓ],:,length(kcut))
+# plot_energy_cuts(kcut,Ecut,lines=[0.0])
--- B0/proj/run_hf.jl
+++ B0/proj/run_hf.jl
@@ -9,13 +9,13 @@ prefix = ARGS[1]
 νstr = round(Int,1000*ν)
 # ------------------ Specification ------------------ #
 lk = 19
-params = Params(ϵ=0.00,Da=00,dθ=1.06π/180,w1=110,w0=77,vf=2482)
+params = Params(ϵ=0.002,Da=-4100,dθ=1.06π/180,w1=110,w0=77,vf=2482)
 initParamsWithStrain(params)
 latt = Lattice()
 initLattice(latt,params;lk=lk)
 
-bm_path = joinpath(fpath,"data/_bm_lk$(lk).jld2")
-hf_path = joinpath(fpath,"data/$(prefix)_hf_$(νstr)_lk$(lk).jld2")
+bm_path = joinpath(fpath,"data/strain_bm_lk$(lk).jld2")
+hf_path = joinpath(fpath,"data/$(prefix)_strain_hf_$(νstr)_lk$(lk).jld2")
 
 # ------------------ Hartree-Fock part ------------------ #
 function compute_hf(ν::Float64,latt::Lattice,params::Params;fname::String="placeholder.txt")
