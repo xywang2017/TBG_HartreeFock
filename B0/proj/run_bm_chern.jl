0a0904dc93ed0dfd37c13a4500b949cae63a393f
Xiaoyu Wang <wxybkaphsc@gmail.com>
2023-07-18 16:16:05 -0400

    abc

--- B0/proj/run_bm_chern.jl
+++ B0/proj/run_bm_chern.jl
@@ -1,24 +1,26 @@
 using PyPlot
-using JLD2
 using Printf
+using JLD2
 fpath = pwd()
-include(joinpath(fpath,"B0/libs/BMChern_mod.jl"))
-include(joinpath(fpath,"B0/libs/plot_helpers.jl"))
+include(joinpath(fpath,"libs/BMChern_mod.jl"))
+include(joinpath(fpath,"libs/plot_helpers.jl"))
 
-twist_angle = 1.20
-_is_strain = "nostrain"
-foldername = @sprintf "%d_%s" round(Int,twist_angle*100) _is_strain
+twist_angle = parse(Float64,ARGS[1])
+_is_strain = ARGS[2]
+lk = parse(Int,ARGS[3])
+
+foldername =@sprintf "%d_%s" round(Int,twist_angle*100) _is_strain 
 # ------------------ Specification ------------------ #
-lk = 16
-params = Params(ϵ=0.00,Da=0.0,φ=0.0*π/180,dθ=twist_angle*π/180,w1=110,w0=77,vf=2482)
+if isequal(_is_strain,"strain")
+    params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=twist_angle*π/180,w1=110,w0=77,vf=2482)
+elseif isequal(_is_strain,"nostrain")
+    params = Params(ϵ=0.000,Da=0.0,φ=0.0*π/180,dθ=twist_angle*π/180,w1=110,w0=77,vf=2482)
+end
 initParamsWithStrain(params)
 latt = Lattice()
 initLattice(latt,params;lk=lk)
 
-bm_path = joinpath(fpath,"$(foldername)/B0/data/bm_lk$(lk).jld2")
-if !isdir(joinpath(fpath,"$(foldername)/B0/data"))
-    mkpath(joinpath(fpath,"$(foldername)/B0/data"))
-end
+bm_path = joinpath(fpath,"$(foldername)/B0/bm_lk$(lk).jld2")
 
 # ------------------ non-interacting part ------------------ #
 function compute_bm(latt::Lattice,params::Params;fname::String="placeholder.txt")
@@ -29,27 +31,3 @@ function compute_bm(latt::Lattice,params::Params;fname::String="placeholder.txt"
 end
 
 bm = compute_bm(latt,params,fname=bm_path);
-
-# ------------------ non-interacting analysis ------------------ #
-
-# kvec = reshape(latt.kvec ./ abs(params.g1),lk,lk)
-# tmp = reshape(load(bm_path,"E"),8,8,lk,lk)
-# ϵ0 = zeros(Float64,4,lk,lk)
-# for i2 in 1:lk, i1 in 1:lk 
-#     ϵ0[1:2,i1,i2] = eigvals(Hermitian(tmp[[1;5],[1;5],i1,i2]))
-#     ϵ0[3:4,i1,i2] = eigvals(Hermitian(tmp[[3;7],[3;7],i1,i2]))
-# end 
-
-# # plot_contour_maps(kvec,ϵ0[1,:,:];points=[params.Kt/abs(params.g1)],contourlines=[100.])
-# plot_contour_maps(kvec,ϵ0[2,:,:];points=ComplexF64[],contourlines=Float64[],limits=Float64[])
-# iΓ = (lk%2==0) ? (lk÷2) : ((lk-1)÷2+1)
-# kcut = real(kvec[:,iΓ])
-# Ecut = reshape(ϵ0[:,:,iΓ],:,length(kcut))
-# plot_energy_cuts(kcut,Ecut,lines=Float64[])
-
-# ----------------- plot real space chern states ------------------- #
-# check that smooth gauge is properly implemented.
-# include(joinpath(fpath,"B0/libs/hybridWannier_mod.jl"));
-# hWS_r = initHybridWannierRealSpace(bm); 
-# # plot_HybridWannier_RealSpace(hWS_r,params);
-# plot_HybridWannier_RealSpaceCombined(hWS_r,params,nk=13);
--- B0/proj/run_hfChern.jl
+++ B0/proj/run_hfChern.jl
@@ -1,30 +1,34 @@
 using PyPlot
-using Printf
 using JLD2
+using Printf
 fpath = pwd()
-include(joinpath(fpath,"B0/libs/HFChern_mod.jl"))
-include(joinpath(fpath,"B0/libs/plot_helpers.jl"))
+include(joinpath(fpath,"libs/HFChern_mod.jl"))
+include(joinpath(fpath,"libs/plot_helpers.jl"))
 
 BLAS.set_num_threads(1)
-
-twist_angle = 1.20
-_is_strain = ARGS[4]
-foldername = @sprintf "%d_%s" round(Int,twist_angle*100) _is_strain
 
 prefix = ARGS[1]
 ν = parse(Float64,ARGS[2])
 νstr = round(Int,1000*ν)
 flag = ARGS[3]
+twist_angle = parse(Float64,ARGS[4])
+_is_strain = ARGS[5]
+lk = parse(Int,ARGS[6])
+foldername =@sprintf "%d_%s" round(Int,twist_angle*100) _is_strain 
 # ------------------ Specification ------------------ #
-lk = 16
-params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=twist_angle*π/180,w1=110,w0=77,vf=2482)
+if isequal(_is_strain,"strain")
+    params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=twist_angle*π/180,w1=110,w0=77,vf=2482)
+elseif isequal(_is+strain,"nostrain")
+    params = Params(ϵ=0.00,Da=0.0,φ=0.0*π/180,dθ=twist_angle*π/180,w1=110,w0=77,vf=2482)
+end
 initParamsWithStrain(params)
 latt = Lattice()
 initLattice(latt,params;lk=lk)
 
-bm_path = joinpath(fpath,"$(foldername)/B0/data/bm_lk$(lk).jld2")
-hf_path = joinpath(fpath,"$(foldername)/B0/data/$(prefix)_$(flag)_hf_$(νstr)_lk$(lk).jld2")
+bm_path = joinpath(fpath,"$(foldername)/B0/bm_lk$(lk).jld2")
+hf_path = joinpath(fpath,"$(foldername)/B0/$(prefix)_$(flag)_hf_$(νstr)_lk$(lk).jld2")
 
+println(hf_path)
 # ------------------ Hartree-Fock part ------------------ #
 function compute_hf(ν::Float64,latt::Lattice,params::Params;fname::String="placeholder.txt")
     hf = HartreeFock()
