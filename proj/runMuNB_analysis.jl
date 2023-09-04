60e51b2d1b865dbe0bcfaf8df8945b159e9a7e1d
Xiaoyu Wang <wxybkaphsc@gmail.com>
2023-09-04 15:08:17 -0400

    abc

---
+++ proj/runMuNB_analysis.jl
@@ -0,0 +1,90 @@
+using PyPlot,JLD2
+fpath = pwd()
+include(joinpath(fpath,"libs/MagneticFieldHF.jl"))
+include(joinpath(fpath,"libs/plot_helpers.jl"))
+#
+# Info and folder name
+# ------------------------------------------------------------------------------ # 
+twist_angle = 128
+foldername = "zeeman/$(twist_angle)_strain"
+params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=twist_angle*0.01*π/180,w1=110,w0=77,vf=2482)
+initParamsWithStrain(params)
+
+
+w0 = "07"
+
+ϕs = [1//8;1//6;1//5;1//4;2//7;1//3;2//5;3//7;1//2]
+ϕs = sort(unique([p//q for q in 1:12 for p in 1:q]))
+ϕs = ϕs[ϕs.<=0.5]
+ϕs = ϕs[2:end]
+sts = []
+for s in -3:3, t in -12:12
+    push!(sts,[s,t])
+    push!(sts,[-s,-t])
+end
+sts = unique(sts)
+
+# -------------------------Streda Line Plot ---------------------------------- # 
+cs = ["r";"g";"b";"c";"m";"darkviolet";"tab:blue";
+        "magenta";"peru";"tab:purple";"tab:olive";"deepskyblue";"seagreen";"gray"]
+fig,ax = subplots(figsize=(5,4))
+# for lines in -4:4
+#     axvline(lines,ls=":",c="gray")
+# end
+for ϕ in ϕs 
+    p,q = numerator(ϕ), denominator(ϕ)
+    gaps = Float64[]
+    μs = Float64[] 
+    fillings = Float64[]
+    for st in sts 
+        s,t = st[1], st[2]
+        νstr = round(Int,1000*(s+t*p/q))
+        if abs(s+t*p/q) < 4 && (s+t*p/q) <=0
+            metadata = joinpath(fpath,"$(foldername)/B/data_w07/_$(p)_$(q)/1_random_init_HF_$(p)_$(q)_nu_$(νstr).jld2")
+            if !isfile(metadata)
+                metadata = joinpath(fpath,"$(foldername)/B/data_w07/_$(p)_$(q)/1_flavor_init_HF_$(p)_$(q)_nu_$(νstr).jld2")
+            end
+            if isfile(metadata)
+                push!(fillings,s+t*p/q)
+                E = load(metadata,"iter_energy")[end]
+                for flag in ["flavor","random","chern","bm","strong","bm_cascade"], seed in 1:10 
+                    metadata0 = joinpath(fpath,"$(foldername)/B/data_w07/_$(p)_$(q)/$(seed)_$(flag)_init_HF_$(p)_$(q)_nu_$(νstr).jld2")
+                    if isfile(metadata0)
+                        E0 = load(metadata0,"iter_energy")[end]
+                        if E0<=E 
+                            E, metadata = E0, metadata0 
+                        end
+                    end
+                end 
+                Δ=computegap(metadata;savename="test.pdf")
+                push!(gaps,Δ)
+                push!(μs,load(metadata,"hf").μ)
+            end
+        end
+    end
+    fillings = round.(fillings,digits=8)
+    idx_sort = sortperm(fillings)
+    fillings, gaps, μs = fillings[idx_sort],gaps[idx_sort], μs[idx_sort]
+    idx = unique(z -> fillings[z], 1:length(fillings))
+    fillings,gaps,μs = fillings[idx], gaps[idx],μs[idx]
+    ns = (fillings .+4)./8
+    for i in eachindex(μs)
+        c_ns = [1 - ns[i],0.5,0.5]
+        # ax.plot([ϕ,ϕ],[μs[i]-gaps[i]/2,μs[i]+gaps[i]/2],"o",c=c_ns,ms=4,markeredgecolor="none")
+        if gaps[i] < 2.0  # only draw gaples states
+            ax.plot([ϕ],[μs[i]],"o",c=c_ns,ms=4,markeredgecolor="none")
+        else # draw states above and below the gap
+            ax.plot([ϕ,ϕ],[μs[i]-gaps[i]/2,μs[i]+gaps[i]/2],"o",c=c_ns,ms=4,markeredgecolor="none")
+        end
+    end
+    # ax.scatter(ones(length(fillings))*ϕ,μs,s=gaps.^2/4,c="k",edgecolor="none")
+end
+# ax.set_facecolor("black")
+# xlim([-4.3,0.3])
+ax.set_xlim([0.0,0.55])
+ax.set_ylabel("E (meV)")
+ax.set_xlabel(L"ϕ/ϕ_0")
+tight_layout()
+# savefig(joinpath(fpath,"$(foldername)/streda_line.png"),transparent=false,dpi=500)
+display(fig)
+close(fig)
--- proj/runStredaGaps.jl
+++ proj/runStredaGaps.jl
@@ -5,7 +5,7 @@ include(joinpath(fpath,"libs/plot_helpers.jl"))
 #
 # Info and folder name
 # ------------------------------------------------------------------------------ # 
-twist_angle = 128
+twist_angle = 105
 foldername = "zeeman/$(twist_angle)_strain"
 params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=twist_angle*0.01*π/180,w1=110,w0=77,vf=2482)
 initParamsWithStrain(params)
@@ -13,10 +13,10 @@ initParamsWithStrain(params)
 
 w0 = "07"
 
-# ϕs = [1//8;1//6;1//5;1//4;2//7;1//3;2//5;3//7;1//2]
-ϕs = sort(unique([p//q for q in 1:12 for p in 1:q]))
-ϕs = ϕs[ϕs.<=0.5]
-ϕs = ϕs[2:end]
+ϕs = [1//8;1//6;1//5;1//4;2//7;1//3;2//5;3//7;1//2]
+# ϕs = sort(unique([p//q for q in 1:12 for p in 1:q]))
+# ϕs = ϕs[ϕs.<=0.5]
+# ϕs = ϕs[2:end]
 sts = []
 for s in -3:3, t in -12:12
     push!(sts,[s,t])
@@ -60,7 +60,7 @@ for ϕ in ϕs
             end
         end
     end
-    # fillings = round.(fillings,digits=8)
+    fillings = round.(fillings,digits=8)
     idx_sort = sortperm(fillings)
     fillings, gaps = fillings[idx_sort],gaps[idx_sort]
     idx = unique(z -> fillings[z], 1:length(fillings))
@@ -133,3 +133,59 @@ for st in sts
     display(fig)
     close(fig)
 end
+
+
+
+# ---------------- non interacting hofstadter spectrum weighted with a given Streda line density matrix
+# plot spectrum 
+function plot_LL_spectrum(s::Int,t::Int,params::Params)
+    foldername0 = "$(twist_angle)_strain"
+    fig,ax = subplots(2,2,sharex=true,sharey=true,figsize=(4,4))
+    strs = ["K","Kprime"]
+    for ϕ in ϕs
+        p, q = numerator(ϕ), denominator(ϕ)
+        νstr = round(Int,1000*(s+t*p/q))
+        metadata = joinpath(fpath,"$(foldername)/B/data_w07/_$(p)_$(q)/1_random_init_HF_$(p)_$(q)_nu_$(νstr).jld2")
+        if !isfile(metadata)
+            metadata = joinpath(fpath,"$(foldername)/B/data_w07/_$(p)_$(q)/1_flavor_init_HF_$(p)_$(q)_nu_$(νstr).jld2")
+        end
+        if isfile(metadata)
+            E = load(metadata,"iter_energy")[end]
+            for flag in ["flavor","random","chern","bm","strong","bm_cascade"], seed in 1:10
+                metadata0 = joinpath(fpath,"$(foldername)/B/data_w$(w0)/_$(p)_$(q)/$(seed)_$(flag)_init_HF_$(p)_$(q)_nu_$(νstr).jld2")
+                if isfile(metadata0)
+                    E0 = load(metadata0,"iter_energy")[end]
+                    if E0<=E 
+                        E, metadata = E0, metadata0 
+                    end
+                end
+            end
+        end
+        hf = load(metadata,"hf");
+        P = reshape(hf.P,2hf.q,hf.nη,hf.ns,2hf.q,hf.nη,hf.ns,hf.nq,hf.q,hf.nq)
+        weights = zeros(Float64,2hf.q,hf.nq,hf.nq)
+        titlestr = ["K↑" "K↓";"K'↑" "K'↓"]
+        for iη in 1:2, is in 1:2
+            str = strs[iη]
+            fname0 = joinpath(fpath,"$(foldername0)/B/data_w07/_$(p)_$(q)/_$(p)_$(q)_$(str)_metadata.jld2")
+            energies = load(fname0,"E");
+            for i2 in 1:hf.nq, i1 in 1:hf.nq, iq in 1:hf.q
+                weights[:,i1,i2] = real(diag(P[:,iη,is,:,iη,is,i1,iq,i2])) .+0.5
+            end
+            ax[iη,is].scatter(ones(length(energies))*ϕ,energies[:].+(3-2is)*p/q*ZeemanUnit(params),marker="o",s=3,edgecolor="none",c=weights[:],vmin=0,vmax=1,cmap="coolwarm")
+            ax[iη,is].set_title(titlestr[iη,is])
+        end
+    end
+    for j in 1:2
+        ax[j,1].set_ylabel("E (meV)")
+        ax[2,j].set_xlabel(L"ϕ/ϕ_0")
+    end
+    ax[1,1].set_xlim([0,0.53])
+    tight_layout() 
+    savefig("tmp.png",dpi=500)
+    display(fig)
+    close(fig)
+    return nothing
+end
+
+plot_LL_spectrum(-3,-1,params)
