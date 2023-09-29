003483add5459985738c0dac84a926edaf43d03b
Xiaoyu Wang <wxybkaphsc@gmail.com>
2023-09-29 19:09:39 -0400

    abc

--- proj/runNonIntHofstadter.jl
+++ proj/runNonIntHofstadter.jl
@@ -14,9 +14,10 @@ w0str = "07"
 ϕs = ϕs[ϕs .> ϕmin]
 ϕs = ϕs[ϕs .<=0.5]
 
+twist_angle = 130
 # calculate spectrum
 function compute_bmLL(ϕ::Rational,str::String,w0::Float64,w0str::String)
-    fname = "NonInt/Hofstadter/110_strain"
+    fname = "NonInt/Hofstadter/$(twist_angle)_strain"
     if !isdir(fname)
         mkpath(fname)
     end
@@ -30,7 +31,7 @@ function compute_bmLL(ϕ::Rational,str::String,w0::Float64,w0str::String)
     println("p= ",p,", q= ",q,", nq= ",nq)
     fname = joinpath(fpath,"$(fname)/_$(p)_$(q)_$(str)_metadata.jld2")
     # fname = ""
-    params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=1.10π/180,w1=110,w0=110*w0,vf=2482)
+    params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=twist_angle*0.01*π/180,w1=110,w0=110*w0,vf=2482)
     initParamsWithStrain(params)
     constructbmLL(bm,params;ϕ= ϕ,nLL=25*q÷p,nq=nq,fname=fname,α=w0, 
         _hBN=false,_strain=true, _σrotation=false, _valley=str,_calculate_overlap=false)
@@ -47,7 +48,7 @@ for ϕ in ϕs
     end
 end
 
-fname = joinpath(fpath,"NonInt/Hofstadter/110_strain/$(str)_NonIntHofstadter_metadata.jld2")
+fname = joinpath(fpath,"NonInt/Hofstadter/$(twist_angle)_strain/$(str)_NonIntHofstadter_metadata.jld2")
 jldopen(fname, "w") do file
     file["hoftstadter_data"] = data
 end
@@ -55,7 +56,7 @@ end
 
 # plot spectrum 
 function plot_LL_spectrum()
-    fname = joinpath(fpath,"NonInt/Hofstadter/132_strain/K_NonIntHofstadter_metadata.jld2")
+    fname = joinpath(fpath,"NonInt/Hofstadter/$(twist_angle)_strain/K_NonIntHofstadter_metadata.jld2")
     data = load(fname,"hoftstadter_data");
     fig = figure(figsize=(2.5,2.5))
     ϕmin = 1//12
@@ -65,15 +66,15 @@ function plot_LL_spectrum()
     for ϕ in ϕs
         p,q = numerator(ϕ), denominator(ϕ)
         energies = reshape(data["$(ϕ)"],2q,:)
-        plot(ones(length(energies[1:(q-p),:]))*ϕ,energies[1:(q-p),:][:],".",ms=3,markeredgecolor="none",color="r")
-        plot(ones(length(energies[(q-p+1):end,:]))*ϕ,energies[(q-p+1):end,:][:],".",ms=3,markeredgecolor="none",color="gray")
+        plot(ones(length(energies[:]))*ϕ,energies[:],".",ms=3,markeredgecolor="none",color="b")
+        # plot(ones(length(energies[1:(q-p),:]))*ϕ,energies[1:(q-p),:][:],".",ms=3,markeredgecolor="none",color="r")
+        # plot(ones(length(energies[(q-p+1):end,:]))*ϕ,energies[(q-p+1):end,:][:],".",ms=3,markeredgecolor="none",color="gray")
     end
     xlim([0,0.55])
-    ylim([-45,45])
     xlabel(L"ϕ/ϕ_0")
     ylabel("E (meV)")
     tight_layout() 
-    savefig("BMLL_132_nostrain.png",dpi=600,transparent=true)
+    # savefig("BMLL_$(twist_angle)_nostrain.png",dpi=600,transparent=true)
     display(fig)
     close(fig)
     return nothing
@@ -83,7 +84,7 @@ plot_LL_spectrum()
 
 # Wannier plot
 function plot_wannier(flag=false)
-    fname = joinpath(fpath,"NonInt/Hofstadter/132_strain/B/NonIntHofstadter_metadata.jld2")
+    fname = joinpath(fpath,"NonInt/Hofstadter/$(twist_angle)_strain/B/NonIntHofstadter_metadata.jld2")
     data = load(fname,"hoftstadter_data");
     ϕmin = 1//40
     ϕs = unique(sort([p//q for q in 1:40 for p in 1:q]))
@@ -115,7 +116,7 @@ function plot_wannier(flag=false)
     tight_layout()
     display(fig)
     if (flag ==true)
-        fname = joinpath(fpath,"Wannier_132_nostrain.png")
+        fname = joinpath(fpath,"Wannier_$(twist_angle)_nostrain.png")
         savefig(fname,dpi=500,transparent=false)
     end
     close(fig)
