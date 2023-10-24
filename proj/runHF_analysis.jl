3bc38ac0df56a4ac0d47d079cda8a13fe326601a
Xiaoyu Wang <wxybkaphsc@gmail.com>
2023-10-24 10:52:17 -0400

    abc

--- proj/runHF_analysis.jl
+++ proj/runHF_analysis.jl
@@ -8,17 +8,17 @@ include(joinpath(fpath,"libs/plot_helpers.jl"))
 # Info and folder name
 # ------------------------------------------------------------------------------ # 
 twist_angles = [105; collect(106:2:138)] 
-twist_angle = 105
+twist_angle = 120
 # for twist_angle in twist_angles
 dir = "/media/xiaoyuw@ad.magnet.fsu.edu/Data/Code/TBG_HartreeFock/"
-dir = "/Volumes/Xiaoyu/Code/TBG_HartreeFock/"
+# dir = "/Volumes/Data/Code/TBG_HartreeFock/"
 # dir = ""
-foldername = dir*"zeeman/$(twist_angle)_strain"
-params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=twist_angle*0.01*π/180,w1=110,w0=77,vf=2482)
+foldername = dir*"zeeman/$(twist_angle)_nostrain"
+params = Params(ϵ=0.00,Da=-4100,φ=0.0*π/180,dθ=twist_angle*0.01*π/180,w1=110,w0=77,vf=2482)
 initParamsWithStrain(params)
 
 # ----------------------------------Hartree Fock spectrum-------------------------------------------- # 
-s,t = -2,-1
+s,t = -3,1
 p,q = 1,8
 νF = (s)+(t)*p/q
 νstr = round(Int,1000*νF)
@@ -31,11 +31,11 @@ metadata = find_lowest_energy_datafile("$(foldername)/_$(p)_$(q)";test_str="_ini
 # plot_density_matrix_bm_valley_spinv0(metadata,ik=1,savename="$(str).png",jj=3,titlestr=str)
 # plot_density_matrix_sublattice_v0(metadata,titlestr=str)
 # end
-plot_density_matrix_bm(metadata,ik=3)
+# plot_density_matrix_bm(metadata,ik=1)
 # end
-# plot_density_matrix_bm_half(metadata,ik=1)
-test_tL2_breaking(metadata)
-plot_density_matrix_global_order_parameters(metadata)
+plot_density_matrix_bm_half(metadata,ik=1)
+# test_tL2_breaking(metadata)
+# plot_density_matrix_global_order_parameters(metadata)
 
 # plot_density_matrix_sublattice(metadata)
 # plot_density_matrix_sublattice_full(metadata)
