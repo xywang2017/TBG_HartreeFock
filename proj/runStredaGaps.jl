f3d2a215cea70dc0aa66316c45803d0e90d20a0d
Xiaoyu Wang <wxybkaphsc@gmail.com>
2023-11-16 16:45:45 -0500

    abc

--- proj/runStredaGaps.jl
+++ proj/runStredaGaps.jl
@@ -7,7 +7,7 @@ dir = "/media/xiaoyuw@ad.magnet.fsu.edu/Data/Code/TBG_HartreeFock/"
 dir = "/Volumes/Data/Code/TBG_HartreeFock/"
 # Info and folder name
 # ------------------------------------------------------------------------------ # 
-twist_angle = 120
+twist_angle = 128
 foldername = dir*"zeeman/$(twist_angle)_strain"
 # params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=twist_angle*0.01*π/180,w1=110,w0=77,vf=2482)
 params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=twist_angle*0.01*π/180,w1=110,w0=77,vf=2482)
@@ -68,7 +68,7 @@ close(fig)
 # -----------------------------Hofstadter spectrum plot ---------------------------- # 
 Δss = []
 sts = [[0,-4],[-1,-3],[-2,-2],[-3,-1]]
-sts = [[0,-3]]
+sts = [[-2,0]]
 for st in sts 
     s,t = st[1], st[2]
     metadatas = String[]
