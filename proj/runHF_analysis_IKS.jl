4a439c849ff520e0b4bec9b1810b44c7efecce80
Xiaoyu Wang <40737927+xywang2017@users.noreply.github.com>
2024-02-02 09:04:38 -0500

    abc

--- proj/runHF_analysis_IKS.jl
+++ proj/runHF_analysis_IKS.jl
@@ -18,13 +18,13 @@ params = Params(ϵ=0.00,Da=-4100,φ=0.0*π/180,dθ=twist_angle*0.01*π/180,w1=11
 initParamsWithStrain(params)
 
 # ----------------------------------Hartree Fock spectrum-------------------------------------------- # 
-s,t = -2,-2
-p,q = 1,8
+s,t = -0.5,-3
+p,q = 1,4
 νF = (s)+(t)*p/q
 νstr = round(Int,1000*νF)
-metadata = find_lowest_energy_datafile("$(foldername)/_$(p)_$(q)";test_str="random_tL_init_HF_$(p)_$(q)_nu_$(νstr)",_printinfo=true)
+metadata = find_lowest_energy_datafile("$(foldername)/_$(p)_$(q)";test_str="init_HF_$(p)_$(q)_nu_$(νstr)",_printinfo=true)
 
-plot_spectra(metadata;savename="test.png")
+# plot_spectra(metadata;savename="test.png")
 plot_density_matrix_bm(metadata,ik=1)
 # test_tL2_breaking(metadata)
 plot_density_matrix_global_order_parameters(metadata)
@@ -95,7 +95,7 @@ mtg = load(mtg_data,"MTG");
 
 fig, ax = subplots(1,1,figsize=(5,8))
 ldos1 = reshape(sum(ldos,dims=3),size(rvec))
-# ldos1 = ldos[:,:,2]
+ldos1 = ldos[:,:,2]
 # ldos2 = ldos[:,:,2]
 ldos1 ./= maximum(ldos1)
 
