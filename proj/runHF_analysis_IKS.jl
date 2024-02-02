07635114e540eb940baccaf3a8885887c9394d35
Xiaoyu Wang <40737927+xywang2017@users.noreply.github.com>
2024-02-02 10:11:22 -0500

    abc

--- proj/runHF_analysis_IKS.jl
+++ proj/runHF_analysis_IKS.jl
@@ -22,7 +22,7 @@ s,t = -0.5,-3
 p,q = 1,4
 νF = (s)+(t)*p/q
 νstr = round(Int,1000*νF)
-metadata = find_lowest_energy_datafile("$(foldername)/_$(p)_$(q)";test_str="_init_HF_$(p)_$(q)_nu_$(νstr)",_printinfo=true)
+metadata = find_lowest_energy_datafile("$(foldername)/_$(p)_$(q)";test_str="random_init_HF_$(p)_$(q)_nu_$(νstr)",_printinfo=true)
 
 plot_spectra(metadata;savename="test.png")
 plot_density_matrix_bm(metadata,ik=1)
@@ -87,7 +87,7 @@ function plot_realspace_cdw(metadata::String,mtg_data::String,ϵ0::Float64;γ::F
 end
 
 μ = load(metadata,"hf").μ
-rvec, ldos  = plot_realspace_cdw(metadata,mtg_data,μ-10.0);
+rvec, ldos  = plot_realspace_cdw(metadata,mtg_data,0.0);
 mtg = load(mtg_data,"MTG");
 
 
