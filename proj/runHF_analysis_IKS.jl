780e0222073f8641ca4bb4b066a586991cd0d9b9
Xiaoyu Wang <40737927+xywang2017@users.noreply.github.com>
2024-02-18 09:08:05 -0500

    abc

--- proj/runHF_analysis_IKS.jl
+++ proj/runHF_analysis_IKS.jl
@@ -19,7 +19,7 @@ initParamsWithStrain(params)
 
 # ----------------------------------Hartree Fock spectrum-------------------------------------------- # 
 s,t = -2,0
-p,q = 1, 8
+p,q = 1, 10
 νF = (s)+(t)*p/q
 νstr = round(Int,1000*νF)
 metadata = find_lowest_energy_datafile("$(foldername)/_$(p)_$(q)";test_str="init_HF_$(p)_$(q)_nu_$(νstr)",_printinfo=true)
