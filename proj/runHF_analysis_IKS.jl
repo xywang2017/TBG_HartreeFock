1730b42676b8a8941672566cec2b1c4aa4f7b835
Xiaoyu Wang <40737927+xywang2017@users.noreply.github.com>
2024-02-20 22:56:15 -0500

    orbital decomposition done
    needs normalization

--- proj/runHF_analysis_IKS.jl
+++ proj/runHF_analysis_IKS.jl
@@ -9,18 +9,18 @@ include(joinpath(fpath,"libs/plot_helpers.jl"))
 # Info and folder name
 # ------------------------------------------------------------------------------ # 
 twist_angles = [105; collect(106:2:138)] 
-twist_angle = 103
+twist_angle = 105
 # for twist_angle in twist_angles
 dir = "/media/xiaoyuw@ad.magnet.fsu.edu/Data/Code/TBG_HartreeFock/zeeman/"
-# dir = "/Volumes/Data/Code/TBG_HartreeFock/"
-dir = "MinHao/"
-foldername = dir*"$(twist_angle)_strain"
+dir = "/Volumes/Data/Code/TBG_HartreeFock/"
+# dir = "MinHao/"
+foldername = dir*"zeeman/$(twist_angle)_strain"
 params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=twist_angle*0.01*π/180,w1=110,w0=77,vf=2482)
 initParamsWithStrain(params)
 
 # ----------------------------------Hartree Fock spectrum-------------------------------------------- # 
 s,t = 0,-2
-p,q = 1, 4
+p,q = 1, 8
 νF = (s)+(t)*p/q
 νstr = round(Int,1000*νF)
 metadata = find_lowest_energy_datafile("$(foldername)/_$(p)_$(q)";test_str="init_HF_$(p)_$(q)_nu_$(νstr)",_printinfo=true)
@@ -32,15 +32,15 @@ plot_density_matrix_global_order_parameters(metadata)
 
 # ----------------------------------IKS Analysis-------------------------------------------- # 
 
-P = reshape(load(metadata,"hf").P,2q,4,2q,4,q,:);
+P = reshape(load(metadata,"hf").P,2q,4,2q,4,:);
 
-p_subblock = P[:,2,:,3,:];
+p_subblock = P[:,4,:,3,:];
 
-_pratio = p_subblock[:,:,1]./ p_subblock[:,:,5]
+_pratio = p_subblock[:,:,1]./ p_subblock[:,:,3]
 
-angle.(_pratio) * 1/(2π) / (3/14)
+angle.(_pratio)
 # ---------------------------- real space density modulation at a given energy ---------------------- # 
-mtg_data = "MinHao/NonInt/103_strain/_$(p)_$(q)_mtg_0_0_metadata.jld2";
+mtg_data = "NonInt/120_strain/_$(p)_$(q)_mtg_0_0_metadata.jld2";
 function plot_realspace_cdw(metadata::String,mtg_data::String,ϵ0::Float64;γ::Float64=0.5)
     hf = load(metadata,"hf");
     U2 = zeros(ComplexF64,size(hf.H));
@@ -50,11 +50,11 @@ function plot_realspace_cdw(metadata::String,mtg_data::String,ϵ0::Float64;γ::F
         U2[:,:,ik] = F.vectors 
         energies[:,ik] = F.values
     end 
-    ldim = size(load(hf.metadata[1],"Vec"),1)
+    ldim = size(load(dir*hf.metadata[1],"Vec"),1)
     U1 = zeros(ComplexF64,ldim*hf.nη*hf.ns,size(hf.H,2),size(hf.H,3))
     U1tmp = reshape(U1,:,hf.nη,hf.ns,2hf.q,hf.nη,hf.ns,size(hf.H,3))
     for iη in 1:hf.nη 
-        jldopen(hf.metadata[iη]) do file 
+        jldopen(dir*hf.metadata[iη]) do file 
             for is in 1:hf.ns 
                 U1tmp[:,iη,is,:,iη,is,:] = reshape(file["Vec"],ldim,2hf.q,:)
             end
@@ -66,32 +66,59 @@ function plot_realspace_cdw(metadata::String,mtg_data::String,ϵ0::Float64;γ::F
         Utot[:,:,ik] = view(U1,:,:,ik) * view(U2,:,:,ik)
     end
 
+    ldosLL = zeros(Float64,size(Utot,1))
+    for ib in eachindex(ldosLL)
+        ldosLL[ib] = sum(  abs2.(Utot[ib,:,:]) ./ ((ϵ0 .- energies).^2 .+ γ^2) )* γ/π
+    end
+    return ldosLL
     ## real space info 
-    mtg = load(mtg_data,"MTG");
-    uvec = reshape(Utot,:,size(hf.H,2),size(hf.H,3));
-    ψ = zeros(ComplexF64,2,mtg.coord.nr,size(hf.H,2),size(hf.H,3));  #layerxsublatticexvalleyxspin , total bands, momentum, position
-    W = reshape(mtg.W,2,:,size(hf.H,3),mtg.coord.nr,hf.nη,1);
+    # mtg = load(mtg_data,"MTG");
+    # uvec = reshape(Utot,:,size(hf.H,2),size(hf.H,3));
+    # ψ = zeros(ComplexF64,2,mtg.coord.nr,size(hf.H,2),size(hf.H,3));  #layerxsublatticexvalleyxspin , total bands, momentum, position
+    # W = reshape(mtg.W,2,:,size(hf.H,3),mtg.coord.nr,hf.nη,1);
 
-    for ik in 1:size(ψ,4), ib in 1:size(ψ,3), ir in 1:mtg.coord.nr, sublatt in 1:2 
-        ψ[sublatt,ir,ib,ik] = sum(view(W,sublatt,:,ik,ir,:,:).*reshape(view(uvec,:,ib,ik),:,hf.nη,hf.ns))
-    end
+    # for ik in 1:size(ψ,4), ib in 1:size(ψ,3), ir in 1:mtg.coord.nr, sublatt in 1:2 
+    #     ψ[sublatt,ir,ib,ik] = sum(view(W,sublatt,:,ik,ir,:,:).*reshape(view(uvec,:,ib,ik),:,hf.nη,hf.ns))
+    # end
 
-    rvec = reshape(mtg.coord.z,mtg.coord.lr,:)
-    # rvec = reshape(mtg.coord.x1,:,1) .+ 1im*reshape(mtg.coord.x2,1,:)
-    ir = reshape(1:length(rvec),size(rvec))
-    ldos = zeros(Float64,size(rvec,1),size(rvec,2),2)
-    for x in 1:size(ldos,1), y in 1:size(ldos,2), jj in 1:2 
-        ldos[x,y,jj] = sum(  abs2.(ψ[jj,ir[x,y],:,:]) ./ ((ϵ0 .- energies).^2 .+ γ^2) )* γ/π
-        # ldos[x,y,jj] = sum(  abs2.(ψ[jj,ir[x,y],:,:]) .* (sign.(hf.μ .- energies) .+1 ) ) / 2
-    end
-
-    return rvec, ldos
+    # rvec = reshape(mtg.coord.z,mtg.coord.lr,:)
+    # # rvec = reshape(mtg.coord.x1,:,1) .+ 1im*reshape(mtg.coord.x2,1,:)
+    # ir = reshape(1:length(rvec),size(rvec))
+    # ldos = zeros(Float64,size(rvec,1),size(rvec,2),2)
+    # for x in 1:size(ldos,1), y in 1:size(ldos,2), jj in 1:2 
+    #     ldos[x,y,jj] = sum(  abs2.(ψ[jj,ir[x,y],:,:]) ./ ((ϵ0 .- energies).^2 .+ γ^2) )* γ/π
+    #     # ldos[x,y,jj] = sum(  abs2.(ψ[jj,ir[x,y],:,:]) .* (sign.(hf.μ .- energies) .+1 ) ) / 2
+    # end
+    # return rvec, ldos
 end
 
 μ = load(metadata,"hf").μ
-rvec, ldos  = plot_realspace_cdw(metadata,mtg_data,5.0);
-mtg = load(mtg_data,"MTG");
+# rvec, ldos  = plot_realspace_cdw(metadata,mtg_data,0.0);
+ldosLL  = plot_realspace_cdw(metadata,mtg_data,μ+3.3);
+# mtg = load(mtg_data,"MTG");
 
+
+# -------------------------------------------------------------------------------------------------------------------------- #
+ldosLL_toplot = reshape(ldosLL,:,2,2,2);
+fig, ax = subplots(2,2,figsize=(5,4),sharex=true,sharey=true)
+lblstr = ["K↑" "K↓";"K'↑" "K'↓";]
+nLL = 25*q÷p
+for η in 1:2, s in 1:2 
+    ax[η,s].plot(0:(nLL-1),ldosLL_toplot[1:2:end,2,η,s]+ldosLL_toplot[1:2:end,1,η,s],"g.",ms=2,label=lblstr[η,s])
+    ax[η,s].plot(-1:-1:-(nLL-1),ldosLL_toplot[2:2:end,2,η,s]+ldosLL_toplot[2:2:end,1,η,s],"g.",ms=2)
+    if η ==2 
+        ax[η,s].set_xlabel("LL index")
+    end
+    if s == 1
+        ax[η,s].set_ylabel("Orbital weight")
+    end
+    ax[η,s].legend()
+    ax[1,1].set_title("1.05 degrees w. strain 1/8")
+    # ax[η,s].set_xlim(-60,60)
+end
+tight_layout()
+display(fig)
+close(fig)
 
 # -------------------------------------------------------------------------------------------------------------------------- #
 
