6e90c3d00de0d04897cf89853a051e19eab00c50
Xiaoyu Wang <wxybkaphsc@gmail.com>
2024-02-26 12:44:54 -0500

    abc

--- proj/runHF_analysis_IKS.jl
+++ proj/runHF_analysis_IKS.jl
@@ -9,18 +9,18 @@ include(joinpath(fpath,"libs/plot_helpers.jl"))
 # Info and folder name
 # ------------------------------------------------------------------------------ # 
 twist_angles = [105; collect(106:2:138)] 
-twist_angle = 105
+twist_angle = 120
 # for twist_angle in twist_angles
-# dir = "/media/xiaoyuw@ad.magnet.fsu.edu/Data/Code/TBG_HartreeFock/zeeman/"
-dir = "/Volumes/Data/Code/TBG_HartreeFock/zeeman/"
+dir = "/media/xiaoyuw@ad.magnet.fsu.edu/Data/Code/TBG_HartreeFock/zeeman/"
+# dir = "/Volumes/Data/Code/TBG_HartreeFock/zeeman/"
 # dir = ""
 foldername = dir*"$(twist_angle)_strain"
 params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=twist_angle*0.01*π/180,w1=110,w0=77,vf=2482)
 initParamsWithStrain(params)
 
 # ----------------------------------Hartree Fock spectrum-------------------------------------------- # 
-s,t = -3, -1
-p,q =  1,12
+s,t = -1, -3
+p,q = 4,11
 νF = (s)+(t)*p/q
 νstr = round(Int,1000*νF)
 metadata = find_lowest_energy_datafile("$(foldername)/_$(p)_$(q)";test_str="init_HF_$(p)_$(q)_nu_$(νstr)",_printinfo=true)
