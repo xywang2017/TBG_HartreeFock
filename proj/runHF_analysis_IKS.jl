6be9eb8ecf340622d0d337db5964e69a821eef06
Xiaoyu Wang <40737927+xywang2017@users.noreply.github.com>
2024-03-03 17:06:39 -0500

    abc

--- proj/runHF_analysis_IKS.jl
+++ proj/runHF_analysis_IKS.jl
@@ -9,7 +9,7 @@ include(joinpath(fpath,"libs/plot_helpers.jl"))
 # Info and folder name
 # ------------------------------------------------------------------------------ # 
 twist_angles = [105; collect(106:2:138)] 
-twist_angle = 132
+twist_angle = 120
 # for twist_angle in twist_angles
 # dir = "/media/xiaoyuw@ad.magnet.fsu.edu/Data/Code/TBG_HartreeFock/zeeman/"
 dir = "/Volumes/Data/Code/TBG_HartreeFock/zeeman/"
@@ -19,16 +19,16 @@ params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=twist_angle*0.01*π/180,w1=1
 initParamsWithStrain(params)
 
 # ----------------------------------Hartree Fock spectrum-------------------------------------------- # 
-s,t = -2, -2
-p,q = 1, 9
+s,t = -1, 0
+p,q = 1, 12
 νF = (s)+(t)*p/q
 νstr = round(Int,1000*νF)
-metadata = find_lowest_energy_datafile("$(foldername)/_$(p)_$(q)";test_str="init_HF_$(p)_$(q)_nu_$(νstr)",_printinfo=true)
+metadata = find_lowest_energy_datafile("$(foldername)/_$(p)_$(q)";test_str="_HF_$(p)_$(q)_nu_$(νstr)",_printinfo=true)
 
 plot_spectra(metadata;savename="test.png")
 plot_density_matrix_bm(metadata,ik=1)
 # test_tL2_breaking(metadata)
-# plot_density_matrix_global_order_parameters(metadata)
+plot_density_matrix_global_order_parameters(metadata)
 
 # ----------------------------------IKS Analysis-------------------------------------------- # 
 nq = 12÷q 
@@ -48,18 +48,18 @@ xlabel(L"q_1")
 ylabel(L"E(q_1)-E(0)\ (meV)")
 xlim([-0.09,1.09])
 tight_layout()
-savefig("test.png",dpi=600)
+# savefig("test.png",dpi=600)
 display(fig)
 close(fig)
 
 
 P = reshape(load(metadata,"hf").P,2q,4,2q,4,:);
 
-p_subblock = P[:,1,:,4,:];
+p_subblock = P[:,1,:,2,:];
 
 _pratio = p_subblock[:,:,2]./ p_subblock[:,:,1]
 
-angle.(_pratio) /(2π) / (1/12)
+angle.(_pratio) /(2π) / (1/11)
 # ---------------------------- real space density modulation at a given energy ---------------------- # 
 mtg_data = "NonInt/120_strain/_$(p)_$(q)_mtg_0_0_metadata.jld2";
 function plot_realspace_cdw(metadata::String,mtg_data::String,ϵ0::Float64;γ::Float64=0.5)
