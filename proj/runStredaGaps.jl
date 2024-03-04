269bdb691ba4551516c2738419805d31a3e70cf7
Xiaoyu Wang <wxybkaphsc@gmail.com>
2024-03-04 12:28:16 -0500

    abc

--- proj/runStredaGaps.jl
+++ proj/runStredaGaps.jl
@@ -8,7 +8,11 @@ dir = "/Volumes/Data/Code/TBG_HartreeFock/"
 # dir = ""
 # Info and folder name
 # ------------------------------------------------------------------------------ # 
+<<<<<<< Updated upstream
 twist_angle = 124
+=======
+twist_angle = 138
+>>>>>>> Stashed changes
 foldername = dir*"zeeman/$(twist_angle)_strain"
 # params = Params(ϵ=0.002,Da=0.0,φ=0.0*π/180,dθ=twist_angle*0.01*π/180,w1=110,w0=77,vf=2125.6)
 params = Params(ϵ=0.002,Da=-4100,φ=0.0*π/180,dθ=twist_angle*0.01*π/180,w1=110,w0=77,vf=2482)
@@ -67,7 +71,11 @@ ax2.set_yticks(collect(5:5:flux_conversion(mx,params)))
 ax2.set_ylabel("B (T)",fontsize=13)
 # scatter([-0.5-3*1/3],[1/3])
 tight_layout()
+<<<<<<< Updated upstream
 savefig("132.png",transparent=false,dpi=600)
+=======
+savefig("138.png",transparent=false,dpi=600)
+>>>>>>> Stashed changes
 # savefig(joinpath(fpath,"$(foldername)/streda_line.png"),transparent=false,dpi=600)
 display(fig)
 close(fig)
